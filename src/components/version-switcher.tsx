edb9308cc9782d1f0ac818ffafbc3fef57ecaa73
kai <luanvuforwork@gmail.com>
2025-11-19 15:03:36 +0700

    Refactor code structure for improved readability and maintainability

--- src/components/version-switcher.tsx
+++ src/components/version-switcher.tsx
@@ -1,16 +1,14 @@
-import { GalleryVerticalEnd } from "lucide-react";
-
 import { SidebarMenu, SidebarMenuItem } from "@/components/ui/sidebar";
 
 export function VersionSwitcher() {
   return (
     <SidebarMenu>
       <SidebarMenuItem className="flex p-4 gap-4 items-center">
-        <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-          <GalleryVerticalEnd className="size-4" />
+        <div className=" text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+          <img src="/logo.png" alt="Quin logo" />
         </div>
         <div className="flex flex-col gap-0.5 leading-none">
-          <span className="font-medium">Gumayoshi</span>
+          <span className="font-medium">Quin Quin</span>
         </div>
       </SidebarMenuItem>
     </SidebarMenu>
